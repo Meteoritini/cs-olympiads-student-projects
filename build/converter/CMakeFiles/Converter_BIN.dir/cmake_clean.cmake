b702e248ef1bd561c39ecf27a1a0e06ec678713c
Jorobraun <JoRoBraun2007@gmail.com>
2025-08-22 13:44:30 +0200

    Push please

---
+++ build/converter/CMakeFiles/Converter_BIN.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Converter_BIN.dir/convert_network.cpp.obj"
+  "CMakeFiles/Converter_BIN.dir/convert_network.cpp.obj.d"
+  "Converter_BIN.exe"
+  "Converter_BIN.exe.manifest"
+  "Converter_BIN.pdb"
+  "libConverter_BIN.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Converter_BIN.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/evaluator/CMakeFiles/Evaluator_BIN.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Evaluator_BIN.dir/evaluator.cpp.obj"
+  "CMakeFiles/Evaluator_BIN.dir/evaluator.cpp.obj.d"
+  "Evaluator_BIN.exe"
+  "Evaluator_BIN.exe.manifest"
+  "Evaluator_BIN.pdb"
+  "libEvaluator_BIN.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Evaluator_BIN.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
